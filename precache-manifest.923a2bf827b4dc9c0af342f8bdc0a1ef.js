32fd4c8cfd25416fbe04b4e1f647f3f63410efb6
amolodushko <sergey.molodushko@kenshoo.com>
2018-12-27 17:15:45 +0200

    Updates

---
+++ precache-manifest.923a2bf827b4dc9c0af342f8bdc0a1ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d97b3a7c427792ecf007",
+    "url": "/deploy/static/css/main.872e7d53.chunk.css"
+  },
+  {
+    "revision": "d97b3a7c427792ecf007",
+    "url": "/deploy/static/js/main.d97b3a7c.chunk.js"
+  },
+  {
+    "revision": "01e5f90d6bba128646db",
+    "url": "/deploy/static/js/1.01e5f90d.chunk.js"
+  },
+  {
+    "revision": "dc0525f6abeba704af90",
+    "url": "/deploy/static/js/runtime~main.dc0525f6.js"
+  },
+  {
+    "revision": "e1e4e99968c0841d0a37afcd8deebf45",
+    "url": "/deploy/static/media/main-bg.e1e4e999.jpg"
+  },
+  {
+    "revision": "fb22826f85fbe2c1f31321689f933958",
+    "url": "/deploy/static/media/bet25-logo.fb22826f.png"
+  },
+  {
+    "revision": "9774139df69e09342ac0ea820bb0e4f5",
+    "url": "/deploy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04beab535164b0d11cbec875c8ac7242.js"
+  "/deploy/precache-manifest.923a2bf827b4dc9c0af342f8bdc0a1ef.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/deploy/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
