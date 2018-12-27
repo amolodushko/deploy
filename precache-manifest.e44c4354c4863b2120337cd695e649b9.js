e1361e79ab552203b8fcd0cc00f469b440ff802a
amolodushko <sergey.molodushko@kenshoo.com>
2018-12-27 13:03:02 +0200

    Updates

---
+++ precache-manifest.e44c4354c4863b2120337cd695e649b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "176f295ddd1988ec9e17",
+    "url": "/deploy/static/css/main.f6d65ccf.chunk.css"
+  },
+  {
+    "revision": "176f295ddd1988ec9e17",
+    "url": "/deploy/static/js/main.176f295d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/deploy/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "dc0525f6abeba704af90",
+    "url": "/deploy/static/js/runtime~main.dc0525f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "081c20b434ef8f30c2db0f59ee14a94c",
+    "url": "/deploy/index.html"
+  }
+];
